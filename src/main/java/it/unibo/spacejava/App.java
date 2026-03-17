030ddd9147203c51380a38f7b1b495c6073b9923
Tomè Alessandro <alessandro.tome@studio.unibo.it>
2026-03-17 10:14:50 +0100

    create branch support and start develop gui

--- src/main/java/it/unibo/spacejava/App.java
+++ src/main/java/it/unibo/spacejava/App.java
@@ -1,10 +1,19 @@
 package it.unibo.spacejava;
 
+import javax.swing.JFrame;
+
+import it.unibo.spacejava.view.GamePanel;
+
 /**
  * Use it for lauch the game.
  */
 public class App {
     public static void main(final String... args) {
-
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        GamePanel panel = new GamePanel(48*16, 48*12);
+        window.add(panel);
+        window.setVisible(true);
     }
 }
