94479cad95979b9b6d52ffd528be838fee14d46e
Tomè Alessandro <alessandro.tome@studio.unibo.it>
2026-05-05 12:49:09 +0200

    Edit GamePanel for PlayerShip and PlayerProjectiles

--- src/main/java/it/unibo/spacejava/view/game/GamePanel.java
+++ src/main/java/it/unibo/spacejava/view/game/GamePanel.java
@@ -17,6 +17,7 @@ package it.unibo.spacejava.view.game;
 import java.awt.Color;
 import it.unibo.spacejava.api.Enemy;
 import it.unibo.spacejava.controller.EnemyProjectileController;
+import it.unibo.spacejava.model.PlayerShip;
 import it.unibo.spacejava.model.ProjectileImpl;
 
 import javax.swing.JPanel;
@@ -32,6 +33,10 @@ public class GamePanel extends JPanel {
     private Image enemyImage;
     private List<Enemy> currentEnemies;
     private Image projectileImage;
+
+    private Image playerImage;
+    private PlayerShip currentPlayer;
+    private List<ProjectileImpl> playerProjectiles;
 
     public GamePanel(int width, int height) {
         super.setSize(width, height);
@@ -61,10 +66,21 @@ public class GamePanel extends JPanel {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            URL imageUrl = getClass().getResource("/skins/spaceShips_001.png");
+            if (imageUrl != null) {
+                playerImage = ImageIO.read(imageUrl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void render(List<Enemy> enemies) {
+    public void render(List<Enemy> enemies, PlayerShip player, List<ProjectileImpl> playerProjectiles) {
         this.currentEnemies = enemies;
+        this.currentPlayer = player;
+        this.playerProjectiles = playerProjectiles;
         repaint(); 
     }
 
@@ -74,6 +90,9 @@ public class GamePanel extends JPanel {
         drawEnemies(g, currentEnemies);
         List<ProjectileImpl> projectiles = EnemyProjectileController.getProjectileList();
         drawEnemyProjectiles(g, projectiles);
+
+        drawPlayer(g);
+        drawPlayerProjectiles(g);
     }
     
     public void drawEnemies(Graphics g, List<Enemy> enemies) {
@@ -98,6 +117,31 @@ public class GamePanel extends JPanel {
                     (int) projectileImpl.getWidth(),
                     (int) projectileImpl.getLenght(),
                     null);
+            }
+        }
+    }
+
+    private void drawPlayer(Graphics g) {
+        if (playerImage != null && currentPlayer != null) {
+            g.drawImage(playerImage,
+                        (int) currentPlayer.getPosition().getX(),
+                        (int) currentPlayer.getPosition().getY(),
+                        (int) currentPlayer.getWidth(),
+                        (int) currentPlayer.getHeight(),
+                        null);
+        }
+    }
+
+    private void drawPlayerProjectiles(Graphics g) {
+        if (playerProjectiles != null && !playerProjectiles.isEmpty()) {
+            g.setColor(Color.CYAN); // i proiettili del giocatore saranno azzurri
+            for (ProjectileImpl p : playerProjectiles) {
+                g.fillRect(
+                    (int) p.getPosition().getX(),
+                    (int) p.getPosition().getY(),
+                    p.getWidth(),
+                    p.getLenght()
+                );
             }
         }
     }
