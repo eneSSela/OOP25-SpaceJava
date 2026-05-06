01fa731a30c20d24a1cca436f174c715e0482a51
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2026-05-06 19:41:39 +0200

    Solve PMD and CheckStyle errore for GameManger interface and its implemetation

--- src/main/java/it/unibo/spacejava/api/GameManger.java
+++ src/main/java/it/unibo/spacejava/api/GameManger.java
@@ -1,18 +1,15 @@
 package it.unibo.spacejava.api;
 
+/**
+ * Questa è l'interfaccia che definisce il controller del gioco, per unire tutti i componenti. Come ad esempio il menu, 
+ * la gestione del suono, la gestione delle onde di nemici e così via.
+ */
+@FunctionalInterface
 public interface GameManger {
 
     /**
-     * this method uses for start the game.
+     * Questo è il metodo dove inizializamo tutti i componenti necasiri al gioco, come la view il controller ed il model del menu.
      */
-    public void startGame();
+    void startGame();
 
-    /**
-     * 
-     */
-    public void pauseGame();
-    
-    public void endGame();
-    
-    public void restartGame();
 }
