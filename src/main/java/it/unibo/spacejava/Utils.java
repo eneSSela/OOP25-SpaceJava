18274ebc4d479d3f57078e284b8749bc81c1b434
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2026-05-10 23:49:21 +0200

    Resolve pmd and checkstyle errors in utlis class

--- src/main/java/it/unibo/spacejava/Utils.java
+++ src/main/java/it/unibo/spacejava/Utils.java
@@ -2,10 +2,25 @@ package it.unibo.spacejava;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.Objects;
 
-public class Utils {
-    public static Image loadImage(String paths) {
-        if (paths == null || paths.isBlank()) {
+/**
+ * Classe di metodi di utility, che fornisce funzionalità comuni.
+ */
+public final class Utils {
+
+    private Utils() {
+        throw new UnsupportedOperationException("Questa è una classe di utilità e non può essere istanziata");
+    }
+
+    /**
+     * Metodo per poter caricare l'immagine dato un percorso.
+     * 
+     * @param paths il percorso dell'immagine da caricare
+     * @return l'immagine carcata, oppure null se il percorso è null o vuoto
+     */
+    public static Image loadImage(final String paths) {
+        if (Objects.isNull(paths) || paths.isBlank()) {
             return null;
         }
         return Toolkit.getDefaultToolkit().getImage(Utils.class.getResource(paths));
