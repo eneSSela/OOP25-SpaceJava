86b74b14105ce2849660e56958c07bb141b94955
Lukas Magalotti <lukas.magalotti@studio.unibo.it>
2026-05-18 16:06:49 +0200

    Copied the isColliding method in the Utils class

--- src/main/java/it/unibo/spacejava/Utils.java
+++ src/main/java/it/unibo/spacejava/Utils.java
@@ -10,4 +10,17 @@ public class Utils {
         }
         return Toolkit.getDefaultToolkit().getImage(Utils.class.getResource(paths));
     }
+
+    /*Metodo helper per controllare se due rettangoli si sovrappongono (AABB).
+    Per implementare la collisione tra i proiettili dei nemici e il giocatore, 
+    useremo un algoritmo molto comune nello sviluppo di giochi in 
+    2D chiamato AABB (Axis-Aligned Bounding Box). In parole povere, immagina 
+    di disegnare un rettangolo invisibile attorno al giocatore e uno attorno 
+    al proiettile: se i due rettangoli si sovrappongono, c'è una collisione.*/
+    public static boolean isColliding(Position pos1, double w1, double h1, Position pos2, double w2, double h2) {
+        return pos1.getX() < pos2.getX() + w2 &&
+               pos1.getX() + w1 > pos2.getX() &&
+               pos1.getY() < pos2.getY() + h2 &&
+               pos1.getY() + h1 > pos2.getY();
+    }
 }
