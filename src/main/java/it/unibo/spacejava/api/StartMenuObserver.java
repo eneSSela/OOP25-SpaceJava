e8f9a9848c79b2971fcc39e5fbccbf498730f68b
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2026-05-19 10:10:54 +0200

    Imlement the interface of the observer

--- src/main/java/it/unibo/spacejava/api/StartMenuObserver.java
+++ src/main/java/it/unibo/spacejava/api/StartMenuObserver.java
@@ -4,11 +4,12 @@ package it.unibo.spacejava.api;
  * Interfaccia per il pattern Observer. 
  * Viene implementata da chiunque voglia essere notificato dei cambiamenti nel StartMenuModel.
  */
+@FunctionalInterface
 public interface StartMenuObserver {
 
     /**
      * Metodo chiamato dal Model quando il suo stato interno cambia 
      * (es. cambio selezione, lampeggio).
      */
-    void update();
+    void updateMenuState();
 }
