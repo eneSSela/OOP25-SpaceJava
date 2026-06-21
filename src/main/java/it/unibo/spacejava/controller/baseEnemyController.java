5253d4ddb154d1f95e8d94d91b5dbe6067a98270
Andrea_Ricci <andrea.ricci60@studio.unibo.it>
2026-06-21 18:45:01 +0200

    Delete base enemy controler

--- src/main/java/it/unibo/spacejava/controller/baseEnemyController.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.spacejava.controller;
-
-/**
- * BaseEnemyController is a class that serves as a base for all enemy controllers in the game. 
- * It can be extended to create specific enemy behaviors and logic.
- */
-public class BaseEnemyController {
-
-}
